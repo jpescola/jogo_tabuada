0da169b5aa7c2ec187662c169a6a36b843a3d56b
João Paulo Lemos Escola <jpescola@gmail.com>
2023-02-17 12:09:00 -0300

    reformulação simplificada

--- main.py
+++ main.py
@@ -1,101 +1,103 @@
 import random
-import sys
 
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QDialog, QApplication
-from PyQt5.uic import loadUi
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
 
 
-class TelaPrincipal(QDialog):
-    v1 = 0
-    v2 = 0
-    r = 0
-    jogadas = 0
-    acertos = 0
+def novo():
+    global r
+    # criando os botões de resposta
+    while True:
+        print('gerando respostas...')
 
-    def __init__(self):
-        super(TelaPrincipal, self).__init__()
-        loadUi('tela.ui', self)
-        jogadas = 0
-        acertos = 0
+        v1 = round(random.random() * 7) + 2
+        v2 = round(random.random() * 7) + 2
+        r = v1 * v2
+        t.lblDesafio.setText(str(v1) + ' x ' + str(v2))
 
-        self.novo()
+        vr = [r, r - v1, r + v2]
+        print(vr)
+        if vr[0] != vr[1] and vr[0] != vr[2] and vr[1] != vr[2]:
+            break
 
-        self.btn1.clicked.connect(self.evento1)
-        self.btn2.clicked.connect(self.evento2)
-        self.btn3.clicked.connect(self.evento3)
+    random.shuffle(vr)
 
-    def acertou(self):
-        print('acertou')
-        self.lblResultado.setText('Acertou! :D')
-        self.jogadas += 1
-        self.acertos += 1
-        self.atualizar()
-        self.novo()
-
-    def errou(self):
-        print('errou')
-        self.lblResultado.setText('Errou :(')
-        self.jogadas += 1
-        self.atualizar()
-        self.novo()
-
-    def atualizar(self):
-        self.lblPontos.setText(str(self.acertos))
-        self.lblJogadas.setText(str(self.jogadas))
-
-    def novo(self):
-        # criando os botões de resposta
-        while True:
-            print('gerando respostas...')
-
-            self.v1 = round(random.random() * 7) + 2
-            self.v2 = round(random.random() * 7) + 2
-            self.r = self.v1 * self.v2
-            self.lblDesafio.setText(str(self.v1) + ' x ' + str(self.v2))
-
-            vr = [self.r, self.r - self.v1, self.r + self.v2]
-            print(vr)
-            if vr[0] != vr[1] and vr[0] != vr[2] and vr[1] != vr[2]:
-                break
-
-        random.shuffle(vr)
-
-        self.btn1.setText(str(vr[0]))
-        self.btn2.setText(str(vr[1]))
-        self.btn3.setText(str(vr[2]))
-
-    def evento1(self):
-        print(self.btn1.text(), self.r, int(self.btn1.text()) == self.r)
-        if int(self.btn1.text()) == self.r:
-            self.acertou()
-        else:
-            self.errou()
-
-    def evento2(self):
-        print(self.btn2.text(), self.r, int(self.btn2.text()) == self.r)
-        if int(self.btn2.text()) == self.r:
-            self.acertou()
-        else:
-            self.errou()
-
-    def evento3(self):
-        print(self.btn3.text(), self.r, int(self.btn3.text()) == self.r)
-        if int(self.btn3.text()) == self.r:
-            self.acertou()
-        else:
-            self.errou()
+    t.btn1.setText(str(vr[0]))
+    t.btn2.setText(str(vr[1]))
+    t.btn3.setText(str(vr[2]))
 
 
-app = QApplication(sys.argv)
-tela_principal = TelaPrincipal()
-w = QtWidgets.QStackedWidget()
-w.addWidget(tela_principal)
-w.setFixedWidth(497)
-w.setFixedHeight(300)
-w.setWindowTitle('Jogo da Tabuada')
-w.show()
-try:
-    sys.exit(app.exec_())
-except:
-    print('Exiting')
+def atualizar():
+    global jogadas, acertos
+    t.lblPontos.setText(str(acertos))
+    t.lblJogadas.setText(str(jogadas))
+
+
+def acertou():
+    global jogadas, acertos
+    print('acertou')
+    t.lblResultado.setText('Acertou! :D')
+    jogadas += 1
+    acertos += 1
+    atualizar()
+    novo()
+
+
+def errou():
+    global jogadas
+    print('errou')
+    t.lblResultado.setText('Errou :(')
+    jogadas += 1
+    atualizar()
+    novo()
+
+
+def evento1():
+    print(t.btn1.text(), r, int(t.btn1.text()) == r)
+    if int(t.btn1.text()) == r:
+        acertou()
+    else:
+        errou()
+
+
+def evento2():
+    print(t.btn2.text(), r, int(t.btn2.text()) == r)
+    if int(t.btn2.text()) == r:
+        acertou()
+    else:
+        errou()
+
+
+def evento3():
+    print(t.btn3.text(), r, int(t.btn3.text()) == r)
+    if int(t.btn3.text()) == r:
+        acertou()
+    else:
+        errou()
+
+
+# inicializando as variáveis
+v1 = 0
+v2 = 0
+r = 0
+jogadas = 0
+acertos = 0
+
+# inicializando a janela
+Form, Window = uic.loadUiType("tela.ui")
+app = QApplication([])
+window = Window()
+t = Form()
+t.setupUi(window)  # carrega os componentes
+
+# define os eventos dos botões
+t.btn1.clicked.connect(evento1)
+t.btn2.clicked.connect(evento2)
+t.btn3.clicked.connect(evento3)
+
+# inicializa o jogo
+novo()
+
+# apresenta a janela
+window.show()
+app.exec()
